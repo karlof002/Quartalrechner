eb6fb02fa76456a4b52facce456b9a6864bac97c
karlof002 <57101995+karlof002@users.noreply.github.com>
2021-02-04 14:33:36 +0100

    Add files via upload

---
+++ Quartalsrechner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Quartalsrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month;
+            int quarter;
+
+            Console.Write("Geben sie bitte ihr Geburtsmonat in Form einer Zahl ein: ");
+            month = Convert.ToInt32(Console.ReadLine());
+
+            if (month >= 1 &&  month <= 12)
+            {
+                if(month <= 3)
+                {
+                    quarter = 1;
+                }
+                else if(month <= 6)
+                {
+                    quarter = 2;
+                }
+                else if(month <= 9)
+                {
+                    quarter = 3;
+                }
+                else
+                {
+                    quarter = 4;
+                }
+
+                Console.WriteLine("Sie wurden im {0}. Quartal geboren.", quarter);
+            }
+            else
+            {
+                Console.WriteLine("Sie haben eine keine Zahl zwischen 1 und 12 eingegeben.");
+            }
+        }
+    }
+}
